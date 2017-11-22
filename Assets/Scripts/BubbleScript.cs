3035e98ee74018bc3cf0dee64c18d1daa0fabf33
Asad khan <asadkhan@Asads-Mac-mini.local>
2017-11-22 17:52:48 +0500

    initial commit

---
+++ Assets/Scripts/BubbleScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleScript : MonoBehaviour {
+
+	Animator anim;
+	private int frame;
+	public GameObject bubble;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Frame: " + frame);
+		frame++;
+
+	}
+
+	void OnMouseDown()
+	{
+		if(Input.GetMouseButton(0))
+		{
+			regenerateBubble ();
+		}
+	}
+
+	private void regenerateBubble ()
+	{
+		anim.Play("bubble_burst",-1,0f);
+		Waiting ();
+		anim.Play("bubbleAnimation");
+		bubble.transform.Translate (GeneratedPosition());
+	}
+
+	IEnumerator Waiting()
+	{
+		Debug.Log("Waiting for bubble to be burst...");
+		yield return new WaitUntil(() => frame >= 10);
+		Debug.Log("Bubble was burst!");
+	}
+
+	Vector3 GeneratedPosition()
+	{
+		int x;
+		x = Random.Range(-3,3);
+		return new Vector3(x,-5,0);
+	}
+}
---
+++ Assets/Scripts/BubblesScipt.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubblesScipt : MonoBehaviour {
+
+	private ParticleSystem ps;
+	public int maxXPosition;
+	public int minXPosition;
+	public int maxYPosition;
+
+	// Use this for initialization
+	void Start () {
+		ps = GetComponent<ParticleSystem>();
+	}
+
+	/**
+	 * @usage It remove the particle that are going outside the boundaries
+	 */
+	void removeTheParticles ()
+	{
+		ParticleSystem.Particle[] particles = new ParticleSystem.Particle[ps.particleCount];
+
+		int num = ps.GetParticles(particles);
+		Debug.Log (num);
+		while (--num >= 0)
+		{
+			if (particles[num].position.x > maxXPosition || 
+				particles[num].position.x < minXPosition || 
+				particles[num].position.y > maxYPosition)
+			{
+				Debug.Log ("Destroying " + num);
+				particles[num].lifetime = 0;
+			}
+		}
+
+		ps.SetParticles(particles, particles.Length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		removeTheParticles();
+	}
+}
